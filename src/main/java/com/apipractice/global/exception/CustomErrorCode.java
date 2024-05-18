f779262e64811bfa569760d40d1a9a1c72850167
lyg0114 <whdnseowkd@naver.com>
2024-05-18 11:23:57 +0200

    feat : CustomExceptionHandler 에  DataIntegrityViolationException 추가

--- src/main/java/com/apipractice/global/exception/CustomErrorCode.java
+++ src/main/java/com/apipractice/global/exception/CustomErrorCode.java
@@ -19,7 +19,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CustomErrorCode {
   // User
-  EMAIL_ALREADY_EXIST(BAD_REQUEST, "이미 존재하는 회원입니다."),
+  EMAIL_ALREADY_EXIST(BAD_REQUEST, "이미 존재하는 메일주소 입니다."),
   NICKNAME_ALREADY_EXIST(BAD_REQUEST, "이미 존재하는 닉네임입니다."),
   LOGIN_FAILED(UNAUTHORIZED, "아이디 또는 비밀번호가 일치하지 않습니다."),
   USER_NOT_FOUND(NOT_FOUND, "사용자를 찾을 수 없습니다."),
@@ -27,6 +27,7 @@ public enum CustomErrorCode {
   // General
   INVALID_HTTP_METHOD(METHOD_NOT_ALLOWED, "잘못된 Http Method 요청입니다."),
   INVALID_VALUE(BAD_REQUEST, "잘못된 입력값입니다."),
+  UNKNON_INVALID_VALUE(BAD_REQUEST, "입력값을 확인해 주세요."),
   SERVER_INTERNAL_ERROR(INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다.");
 
   private final HttpStatus httpStatus;
--- src/main/java/com/apipractice/global/exception/CustomExceptionHandler.java
+++ src/main/java/com/apipractice/global/exception/CustomExceptionHandler.java
@@ -2,10 +2,12 @@ package com.apipractice.global.exception;
 
 import static com.apipractice.global.exception.CustomErrorCode.INVALID_VALUE;
 import static com.apipractice.global.exception.CustomErrorCode.SERVER_INTERNAL_ERROR;
+import static com.apipractice.global.exception.CustomErrorCode.UNKNON_INVALID_VALUE;
 
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -40,7 +42,7 @@ public class CustomExceptionHandler {
       HttpServletRequest request
   ) {
     String validationMessage = Objects.requireNonNull(ex.getFieldError()).getDefaultMessage();
-    log.error("[MethodArgumentNotValidException] url: {} | errorCode: {} | errorMessage: {} | cause Exception: ",
+    log.error( "[MethodArgumentNotValidException] url: {} | errorCode: {} | errorMessage: {} | cause Exception: ",
         request.getRequestURL(), INVALID_VALUE, validationMessage, ex);
 
     CustomException customException = new CustomException(INVALID_VALUE, validationMessage);
@@ -49,13 +51,28 @@ public class CustomExceptionHandler {
         .body(new ErrorResponse(customException));
   }
 
+  // 데이터 정합성이 맞지 않을때
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(
+      DataIntegrityViolationException ex,
+      HttpServletRequest request
+  ) {
+    log.error("[Common Exception] url: {} | errorMessage: {} | Exception ex: {}",
+        request.getRequestURL(), ex.getMessage(), ex.getClass().getName());
+
+    CustomException customException = new CustomException(INVALID_VALUE, "입력 값이 잘못 되었습니다.");
+    return ResponseEntity
+        .status(INVALID_VALUE.getHttpStatus())
+        .body(new ErrorResponse(customException));
+  }
+
   // Other Error
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ErrorResponse> exception(Exception e, HttpServletRequest request) {
-    log.error("[Common Exception] url: {} | errorMessage: {}",
-        request.getRequestURL(), e.getMessage());
+    log.error("[Common Exception] url: {} | errorMessage: {} | Exception ex: {}",
+        request.getRequestURL(), e.getMessage(), e.getClass().getName());
     return ResponseEntity
-        .status(SERVER_INTERNAL_ERROR.getHttpStatus())
-        .body(new ErrorResponse(SERVER_INTERNAL_ERROR));
+        .status(INVALID_VALUE.getHttpStatus())
+        .body(new ErrorResponse(UNKNON_INVALID_VALUE));
   }
 }
