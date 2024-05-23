2998d2c3587cd42f6628d8cccc693ceddc313e6a
lyg0114 <whdnseowkd@naver.com>
2024-05-23 18:08:21 +0200

    docs : 주석 추가

--- src/main/java/com/apipractice/global/exception/CustomException.java
+++ src/main/java/com/apipractice/global/exception/CustomException.java
@@ -13,7 +13,10 @@ public class CustomException extends RuntimeException {
   private final CustomErrorCode errorCode;
   private final String errorMessage;
 
-  // Without Cause Exception
+  /**
+   * @param errorCode
+   * - cause 없이 생성
+   */
   public CustomException(CustomErrorCode errorCode) {
     super(errorCode.getErrorMessage());
     this.errorCode = errorCode;
@@ -26,7 +29,11 @@ public class CustomException extends RuntimeException {
     this.errorMessage = errorMessage;
   }
 
-  // With Cause Exception
+  /**
+   * @param errorCode
+   * @param cause
+   *  - 로깅을 위해 실제 원인이 되는 cause와 함께 생성
+   */
   public CustomException(CustomErrorCode errorCode, Exception cause) {
     super(errorCode.getErrorMessage(), cause);
     this.errorCode = errorCode;
