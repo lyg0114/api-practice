ce2cc2d7337498b3824e638c49882ce42e97ec1f
lyg0114 <whdnseowkd@naver.com>
2024-05-23 17:43:54 +0200

    test : 패스워드 인코딩 학습테스트 주석 추가

--- src/test/java/com/study/PasswordEncodingTest.java
+++ src/test/java/com/study/PasswordEncodingTest.java
@@ -2,6 +2,7 @@ package com.study;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,15 +18,17 @@ public class PasswordEncodingTest {
     return new BCryptPasswordEncoder();
   }
 
+  @DisplayName("패스워드 인코딩 테스트")
   @Test
-  void encodingTest(){
+  void encodingTest() {
     PasswordEncoder encoder = passwordEncoder();
     String encode = encoder.encode("test1234");
     System.out.println("encode = " + encode);
   }
 
+  @DisplayName("패스워드 검증 테스트")
   @Test
-  void decodingTest(){
+  void decodingTest() {
     PasswordEncoder encoder = passwordEncoder();
     String encodedPw = "$2a$10$V1xwB.9uBw0O3UUoIl9CauPsPFm14vLL5/St50dlZ9JHe.k6pI9Wq";
     assertThat(encoder.matches("test1234", encodedPw)).isTrue();
