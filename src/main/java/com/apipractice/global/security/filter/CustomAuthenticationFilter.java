268e59b04df046760e64baa7a05dc2f79337ae69
lyg0114 <whdnseowkd@naver.com>
2024-05-24 22:14:28 +0200

    feat : CustomAuthenticationFilter 예외처리 및 주석추가

--- src/main/java/com/apipractice/global/security/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/apipractice/global/security/filter/CustomAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.apipractice.global.security.filter;
 
+import static com.apipractice.global.exception.CustomErrorCode.INVALID_VALUE;
+
 import com.apipractice.domain.member.dto.MemberDto.LoginRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.stream.Collectors;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -20,7 +23,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  * @package : com.apipractice.global.security.filter
  * @since : 20.05.24
  *
- *  - id, pw를 이용한 사용자 체크 필터
+ *  - id, pw 를 이용한 사용자 체크 필터
  */
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -41,6 +44,13 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     this.objectMapper = new ObjectMapper();
   }
 
+  /**
+   * @param request
+   * @param response
+   * @return authentication
+   * @throws AuthenticationException
+   *  - Provider에서 사용할 Authentication 객체 생성 및 반환
+   */
   @Override
   public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
       throws AuthenticationException {
@@ -49,14 +59,30 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     return authenticationManager.authenticate(authentication);
   }
 
+  /**
+   * @param request
+   * @return
+   *   - json으로부터 LoginRequest 객체 생성 및 반환
+   */
   public LoginRequest createLoginRequest(HttpServletRequest request) {
     try {
-      return objectMapper.readValue(
-          request.getReader().lines().collect(Collectors.joining(System.lineSeparator())),
-          LoginRequest.class);
+      return objectMapper.readValue(getUserInfo(request), LoginRequest.class);
     } catch (IOException e) {
-      //TODO : 적절한 예외를 반환할 수 있도록 처리
-      throw new RuntimeException(e);
+      throw new AuthenticationServiceException(INVALID_VALUE.getErrorMessage(), e);
     }
+  }
+
+  /**
+   * @param request
+   * @return
+   * @throws IOException
+   *
+   *   -  request 로 부터 JSON stirng 추출
+   */
+  private String getUserInfo(HttpServletRequest request) throws IOException {
+    return request
+        .getReader()
+        .lines()
+        .collect(Collectors.joining(System.lineSeparator()));
   }
 }
